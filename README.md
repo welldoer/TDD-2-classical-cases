7871a0c04a6fe25818913814f90e4db103eadb54
welldoer <welldoer@263.net>
2019-01-16 17:42:31 +0800

    :construction_worker: 准备初始可运行的环境
    1) 运行 Gradle init，安装 Gradle wrapper
    2) 准备 Travis、SonarCloud 相关配置
    3) 编写第一个空白测试代码

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TDD-2-classical-cases
+
+
+#### 待开发任务清单
+
+~~1. 在GitHub中新建repo~~
+
+~~2. 在本地Eclipse开发环境git Clone，并加载Gradle环境~~
+
+~~3. 配置 Travis-CI、SonarCloud 环境~~
+
---
+++ src/test/java/net/blogjava/primeFactors/PrimeFactorsTest.java
@@ -0,0 +1,18 @@
+package net.blogjava.primeFactors;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrimeFactorsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
