9ea250f207c9432c36d74093272d79155b49daa6
welldoer <welldoer@263.net>
2019-01-16 23:58:43 +0800

    :hammer: 准备增加一个包含补中的测试，但发现有必要先重构代码
    1）添加补中测试后屏蔽
    2）修改代码，将每次的击中情况记录下来

--- src/test/java/net/blogjava/bowling_game/BowlingGameTest.java
+++ src/test/java/net/blogjava/bowling_game/BowlingGameTest.java
@@ -26,6 +26,15 @@ public class BowlingGameTest {
 		assertEquals( 20, game.score() );
 	}
 
+//	@Test
+	public void testOneSpare() {
+		game.roll( 5 );
+		game.roll( 5 );			// Spare
+		game.roll( 3 );
+		rollMany( 17, 0 );
+		assertEquals( 16, game.score() );
+	}
+
 	private void rollMany( int n, int pins ) {
 		for( int i = 0; i < n; i++ )
 			game.roll( pins );
