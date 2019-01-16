1c493c6325e77667da5aae66fc9dc937f09ada00
welldoer <welldoer@263.net>
2019-01-16 23:05:13 +0800

    :white_check_mark: 编写保龄球游戏的初始测试
    1）搭建基本的测试骨架

---
+++ src/main/java/net/blogjava/bowling_game/Game.java
@@ -0,0 +1,13 @@
+package net.blogjava.bowling_game;
+
+public class Game {
+
+	public void roll( int pins ) {
+		
+	}
+
+	public int score() {
+		return 0;
+	}
+
+}
---
+++ src/test/java/net/blogjava/bowling_game/BowlingGameTest.java
@@ -0,0 +1,24 @@
+package net.blogjava.bowling_game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BowlingGameTest {
+	
+	private Game game;
+
+	@Before
+	public void setUp() throws Exception {
+		game = new Game();
+	}
+
+	@Test
+	public void testGutterGame() {
+		for( int i = 0; i < 20; i++ )
+			game.roll( 0 );
+		assertEquals( 0, game.score() );
+	}
+
+}
