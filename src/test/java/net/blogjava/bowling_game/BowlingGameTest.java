3cfeb4a2b0e8e50f6992706bd12f71c53097902e
welldoer <welldoer@263.net>
2019-01-16 23:35:03 +0800

    :hammer: 优化代码
    1）提取方法

--- src/test/java/net/blogjava/bowling_game/BowlingGameTest.java
+++ src/test/java/net/blogjava/bowling_game/BowlingGameTest.java
@@ -16,16 +16,19 @@ public class BowlingGameTest {
 
 	@Test
 	public void testGutterGame() {
-		for( int i = 0; i < 20; i++ )
-			game.roll( 0 );
+		rollMany( 20, 0 );
 		assertEquals( 0, game.score() );
 	}
 
 	@Test
 	public void testAllOnes() {
-		for( int i = 0; i < 20; i++ )
-			game.roll( 1 );
+		rollMany( 20, 1 );
 		assertEquals( 20, game.score() );
+	}
+
+	private void rollMany( int n, int pins ) {
+		for( int i = 0; i < n; i++ )
+			game.roll( pins );
 	}
 
 }
