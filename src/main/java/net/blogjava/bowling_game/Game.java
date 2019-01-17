5e85187c51c93aa9ce4120eb2bd44a6d3756db5d
welldoer <welldoer@263.net>
2019-01-17 10:45:45 +0800

    :white_check_mark: 增加全中的测试
    1）快速通过测试

--- src/main/java/net/blogjava/bowling_game/Game.java
+++ src/main/java/net/blogjava/bowling_game/Game.java
@@ -13,7 +13,10 @@ public class Game {
 		int score = 0;
 		int frameIndex = 0;
 		for( int frame = 0; frame < 10; frame++ ) {
-			if( isSpare(frameIndex) ) {
+			if( rolls[ frameIndex ] == 10 ) {			// Strike
+				score += 10 + rolls[ frameIndex + 1 ] + rolls[ frameIndex + 2 ];
+				frameIndex++;
+			} else if( isSpare(frameIndex) ) {
 				score += 10 + rolls[ frameIndex + 2 ];
 				frameIndex += 2;
 			} else {
