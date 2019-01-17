d8761df95477edf86a81356b56d7d89cc71504c0
welldoer <welldoer@263.net>
2019-01-17 11:01:35 +0800

    :hammer: 重构代码
    1）按业务含义提取方法

--- src/main/java/net/blogjava/bowling_game/Game.java
+++ src/main/java/net/blogjava/bowling_game/Game.java
@@ -14,17 +14,29 @@ public class Game {
 		int frameIndex = 0;
 		for( int frame = 0; frame < 10; frame++ ) {
 			if( rolls[ frameIndex ] == 10 ) {			// Strike
-				score += 10 + rolls[ frameIndex + 1 ] + rolls[ frameIndex + 2 ];
+				score += 10 + strikeBonus( frameIndex );
 				frameIndex++;
 			} else if( isSpare(frameIndex) ) {
-				score += 10 + rolls[ frameIndex + 2 ];
+				score += 10 + spareBonus( frameIndex );
 				frameIndex += 2;
 			} else {
-				score += rolls[ frameIndex ] + rolls[ frameIndex + 1 ];
+				score += sumOfBallsInFrame( frameIndex );
 				frameIndex += 2;
 			}
 		}
 		return score;
+	}
+	
+	private int strikeBonus( int frameIndex ) {
+		return rolls[ frameIndex + 1 ] + rolls[ frameIndex + 2 ];
+	}
+
+	private int spareBonus( int frameIndex ) {
+		return rolls[ frameIndex + 2 ];
+	}
+
+	private int sumOfBallsInFrame( int frameIndex ) {
+		return rolls[ frameIndex ] + rolls[ frameIndex + 1 ];
 	}
 
 	private boolean isSpare(int frameIndex) {
